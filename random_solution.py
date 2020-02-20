948dd0cf9eaf063d4ee3dc97ac83f5e11d3ff2bf
Danil Doroshin <ddddanil5555@gmail.com>
2020-02-20 22:01:05 +0300

    Random v2

--- random_solution.py
+++ random_solution.py
@@ -1,14 +1,19 @@
 from random import randint
 from test import Solution, print_solutions
-import random
+from random import shuffle
 from logging import getLogger
 logger = getLogger("Task.random_solution")
 
 def sample_ans(input_):
     world, books, libraries = input_
     ans = []
-    for lib in random.shuffle(libraries):
-        ans.append( (lib.id, random.shuffle(list(lib.books))) )
+    # print(f"Got w {world} b {books} l {libraries}")
+    shuffle(libraries)
+    for lib in libraries:
+        lib_books = list(lib.books)
+        shuffle(lib_books)
+        ans.append( (lib.id, lib_books) )
+    print(f"Got ans {ans}")
     return ans
 
 
