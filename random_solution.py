bd52e62f7d024fa1ae57e041ed708ffe224ad267
Danil Doroshin <ddddanil5555@gmail.com>
2020-02-20 21:20:42 +0300

    Random solution v1

--- random_solution.py
+++ random_solution.py
@@ -1,10 +1,16 @@
 from random import randint
 from test import Solution, print_solutions
+import random
 from logging import getLogger
 logger = getLogger("Task.random_solution")
 
 def sample_ans(input_):
-    pass
+    world, books, libraries = input_
+    ans = []
+    for lib in random.shuffle(libraries):
+        ans.append( (lib.id, random.shuffle(list(lib.books))) )
+    return ans
+
 
 solution = Solution(sample_ans, 100, "random_solution")
 if __name__ == "__main__":
