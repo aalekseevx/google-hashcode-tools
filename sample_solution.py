d1a7ab6f6a6bdcbfd9c439f9b5c82081d1b8c4cf
Alekseev Aleksandr <alekseev2002@inbox.ru>
2020-02-20 16:25:17 +0300

    Added hashcode template

---
+++ sample_solution.py
@@ -0,0 +1,11 @@
+from random import randint
+from test import Solution, print_solutions
+from logging import getLogger
+logger = getLogger("Task.random_solution")
+
+def sample_ans(input_):
+    pass
+
+solution = Solution(sample_ans, 100, "random_solution")
+if __name__ == "__main__":
+    print_solutions([solution])
---
+++ utils.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+from typing import List, Tuple
+from logging import getLogger
+logger = getLogger("Task.utils")
+
+tasks = {
+    'a': "",
+    'b': "",
+    'c': "",
+    'd': "",
+    'e': "",
+}
+
+## IO   ##
+
+def read_test(testfile: str):
+    pass
+
+
+def read_answer(answerfile: str, cars: int):
+   pass
+
+
+def write_answer(ans, answerfile: str):
+    pass
+
+
+## Score  ##
+
+def score(input_, answer) -> int:
+    pass
