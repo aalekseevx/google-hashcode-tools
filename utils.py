7aad264b26454833a4d6ea8b2fd98d97b52429cc
Danil Doroshin <ddddanil5555@gmail.com>
2020-02-20 17:10:37 +0300

    utils (forgot)

--- utils.py
+++ utils.py
@@ -4,28 +4,39 @@ from logging import getLogger
 logger = getLogger("Task.utils")
 
 tasks = {
-    'a': "",
-    'b': "",
-    'c': "",
-    'd': "",
-    'e': "",
+    'a': "a_example.in",
+    'b': "b_small.in",
+    'c': "c_medium.in",
+    'd': "d_quite_big.in",
+    'e': "e_also_big.in",
 }
 
 ## IO   ##
 
 def read_test(testfile: str):
-    pass
-
-
-def read_answer(answerfile: str, cars: int):
-   pass
+    with open(testfile, "r") as fin:
+    	numbers = list(map(int, fin.read().split()))
+    	M, N = numbers[0], numbers[1]
+    	return {
+    		"M": M,
+    		"pizza": numbers[2:]
+    	}
 
 
 def write_answer(ans, answerfile: str):
-    pass
+    with open(answerfile, "w") as fout:
+    	if (len(ans) < 100):
+    		print(ans)
+    	fout.write(f"{len(ans)}\n")
+    	fout.write(" ".join(map(str, ans)))
 
 
 ## Score  ##
 
 def score(input_, answer) -> int:
-    pass
+	ans = sum([input_['pizza'][i] for i in answer])
+	if ans > input_['M']:
+		logger.error("you lost")
+		return 0
+	else:
+		return ans
