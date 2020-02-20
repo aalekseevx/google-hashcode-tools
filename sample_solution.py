778b01b9bc3f67ce9998d35cee35c9e1eca49ca7
Danil Doroshin <ddddanil5555@gmail.com>
2020-02-20 17:08:24 +0300

    Practice solution

--- sample_solution.py
+++ sample_solution.py
@@ -4,7 +4,13 @@ from logging import getLogger
 logger = getLogger("Task.random_solution")
 
 def sample_ans(input_):
-    pass
+    sm = 0
+    ans = []
+    for i, sl in enumerate(input_['pizza']):
+        if sm + sl <= input_['M']:
+            ans.append(i)
+            sm += sl
+    return ans
 
 solution = Solution(sample_ans, 100, "random_solution")
 if __name__ == "__main__":
