930c00e8258f75262bc0ba4456b514514c5457c8
Danil Doroshin <ddddanil5555@gmail.com>
2020-02-20 22:17:20 +0300

    Score v2

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 from collections import namedtuple
 from typing import List, Tuple
-from itertools import tee, izip
 import math
 from logging import getLogger
 logger = getLogger("Task.utils")
@@ -18,10 +17,9 @@ World = namedtuple("World", ["books", "libraries", "days"])
 Library = namedtuple("Library", ["id", "signup", "daily", "books"])
 
 def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    next(b, None)
-    return izip(a, b)
+    a = next(iterable)
+    b = next(iterable, None)
+    return (a, b)
 
 ## IO   ##
 
@@ -32,39 +30,52 @@ def read_test(testfile: str):
     with open(testfile, "r") as f:
         line1 = f.readline()
         world = World._make( map(int, line1.split()) )
+        # print(f"World {world}")
         line2 = f.readline()
         for score in map(int, line2.split()):
             books.append(score)
-        for i, line in enumerate(pairwise(f.readlines())):
-            nums = map(int, line[0].split())
-            book_set = map(int, line[1].split())
-            libraries.append(Library._make( [ i, nums[1], nums[2], set(book_set) ] ))
+        # print(f"Book scores {books}")
+        i = 0
+        while line1 := f.readline():
+            line2 = f.readline()
+            lib_info = list(map(int, line1.split()))
+            book_set = set(list(map(int, line2.split())))
+            new_library = Library._make( [ i, lib_info[1], lib_info[2], book_set ] )
+            # print(f"Library {new_library}")
+            libraries.append(new_library)
+            i += 1
     return world, books, libraries
 
 
 def write_answer(ans, answerfile: str):
-    with open(answerfile) as f:
-        f.writeline(f"{len(ans)}")
+    with open(answerfile, 'w') as f:
+        f.write(f"{len(ans)}\n")
         for lib_id, books in ans:
-            f.writeline(f"{lib_id} {len(books)}\n{' '.join(books)}")
+            f.write(f"{str(lib_id)} {len(books)}\n{' '.join(map(str,books))}\n")
 
 
 ## Score  ##
 
 def score(input_, answer) -> int:
     world, books, libraries = input_
+    # print(f"Got w {world} b {books} l {libraries}")
     current_time = 0
     score = 0
     scanned = set()
-    for lib_id, books in answer:
+    for lib_id, ans_books in answer:
         current_time += libraries[lib_id].signup
         if current_time >= world.days:
             break
         library = libraries[lib_id]
-        scan_time = min(world.days - current_time, math.ceil(len(books) / library.daily))
-        scanned_books = min(library.daily * scan_time, len(books))
-        for book in books[:scanned_books]:
+        scan_time = min(world.days - current_time, math.ceil(len(ans_books) / library.daily))
+        scanned_books_len = min(library.daily * scan_time, len(ans_books))
+        scanned_books = ans_books[:scanned_books_len]
+        # print(f"scanning {scanned_books}, lib books {library.books}")
+        if not set(scanned_books) <= library.books:
+            logger.error(f"Scanned books that are not in library")
+        for book in scanned_books:
             scanned.add(book)
+    # print(f"Got books {list(scanned)}\nglobal {books}")
     for book in scanned:
         score += books[book]
     return score
